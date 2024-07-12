1fc864ed75af7103e7393879160411bb58b53556
iMac <listovey>
2024-07-12 18:39:31 +0900

    ..

--- src/component/Header.js
+++ src/component/Header.js
@@ -13,7 +13,7 @@ function HeaderComponent(props) {
         <Head>
             <title>{title}</title>
             <link rel="icon" href={`/favicon.ico`} />
-            <link rel="canonical" href={process.env.NEXT_PUBLIC_WWW_ORIGIN}/>
+            <link rel="canonical" href={process.env.NEXT_PUBLIC_ORIGIN}/>
             <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no" />
             <meta name="keywords" content={keywords}/>
             <meta  name="description" content={description}/>
@@ -22,7 +22,7 @@ function HeaderComponent(props) {
             <meta property="og:locale:alternate" content="en_US"/>
             <meta property="og:title" content={title}/>
             <meta property="og:image" content={thumbnail}/>
-            <meta property="og:url" content={`${process.env.NEXT_PUBLIC_WWW_ORIGIN}${path}`}/>
+            <meta property="og:url" content={`${process.env.NEXT_PUBLIC_ORIGIN}${path}`}/>
             <meta property="og:type" content="website"/>
             <meta name="twitter:title" content={title}/>
             <meta name="twitter:description" content={description}/>
